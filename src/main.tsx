3c35e1aa95cb4f526c3f316d1aa1f433c8e6f7dc
Ricardo-CBQA <140541333+Ricardo-CBQA@users.noreply.github.com>
2023-08-14 13:45:16 -0600

    add: form finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { store } from '@app/store';
+import { ConfigProvider } from 'antd';
+import esES from 'antd/locale/es_ES';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
@@ -9,6 +11,8 @@ import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <App />
+    <ConfigProvider locale={esES}>
+      <App />
+    </ConfigProvider>
   </Provider>,
 );
