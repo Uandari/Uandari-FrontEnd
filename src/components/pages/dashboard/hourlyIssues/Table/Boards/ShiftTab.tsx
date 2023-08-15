21141351c63132de2f7c8ba9c4e83e8908c92b51
Ricardo-CBQA <140541333+Ricardo-CBQA@users.noreply.github.com>
2023-08-15 16:01:36 -0600

    add: cells added to each shift on the board

--- src/components/pages/dashboard/hourlyIssues/Table/Boards/ShiftTab.tsx
+++ src/components/pages/dashboard/hourlyIssues/Table/Boards/ShiftTab.tsx
@@ -1,14 +1,36 @@
 import ClockIcon from '@assets/clock-two 1.svg';
+import { Select } from 'antd';
 
 export type ShiftTabProps = {
   shift: string;
 };
 
+const shiftsLabels = [
+  {
+    value: 'Célula 1',
+    label: 'Célula 1',
+  },
+  {
+    value: 'Célula 2',
+    label: 'Célula 2',
+  },
+  {
+    value: 'Célula 3',
+    label: 'Célula 3',
+  },
+];
+
 function ShiftTab({ shift }: ShiftTabProps) {
   return (
-    <div className="flex items-center gap-2 px-4 py-2 border-b border-main_color">
-      <img src={ClockIcon} alt="clock-icon" />
-      <p className="text-main_text_color text-sm"> {shift} </p>
+    <div className="flex justify-between items-center px-4 py-2 border-b border-main_color">
+      <div className="flex items-center gap-2">
+        <img src={ClockIcon} alt="clock-icon" />
+        <p className="text-main_text_color text-sm"> {shift} </p>
+      </div>
+      <Select
+        placeholder="Selecciona la célula de este turno"
+        options={shiftsLabels}
+      />
     </div>
   );
 }
