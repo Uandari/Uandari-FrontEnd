fa8e9526064059a9698339120e048241a1e6b303
Ricardo-CBQA <140541333+Ricardo-CBQA@users.noreply.github.com>
2023-08-27 21:42:27 -0600

    fix: padding added

--- src/components/pages/dashboard/hourlyIssues/modules/Tabs/LabelTab.tsx
+++ src/components/pages/dashboard/hourlyIssues/modules/Tabs/LabelTab.tsx
@@ -7,11 +7,14 @@ export type LabelTabProps = {
 function LabelTab({ title, icon, quantity }: LabelTabProps) {
   return (
     <div className="flex w-full items-center justify-between pb-2 border-b border-main_color">
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center px-2">
         <img className="w-5" src={icon} alt="" />
         <p className="text-main_text_color"> {title} </p>
       </div>
-      <p className="font-medium text-main_text_color text-lg"> {quantity} </p>
+      <p className="font-medium text-main_text_color text-lg px-2">
+        {' '}
+        {quantity}{' '}
+      </p>
     </div>
   );
 }
