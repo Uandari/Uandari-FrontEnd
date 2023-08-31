f096c0138323f78d4e9b99a559f8f0286f48dd1a
Andrés Cordero <andresmcorderor@gmail.com>
2023-08-31 03:11:07 -0600

    feature: assistence per shift in progress

--- src/components/pages/dashboard/assistence-per-shift/modules/RowBoard.tsx
+++ src/components/pages/dashboard/assistence-per-shift/modules/RowBoard.tsx
@@ -1,54 +1,31 @@
-import ColorChangingBox from "./Tabs/ColorChangingBox";
-
+screenTop
 export type RowBoardProps = {
-  month: string;
-  days: number;
+  shift: string;
 };
 
-export default function RowBoard({ month, days }: RowBoardProps) {
-  const cellBoards = [];
-
-  for (let i = 0; i < days + 1; i++) {
-    cellBoards.push(
-      <ColorChangingBox key={i}></ColorChangingBox>
-    );
-  }
-
+export default function RowBoard({ shift }: RowBoardProps) {
   return (
     <div className="border-b border-main_color ">
-      <div className="flex  grid-cols-4 ">
-        <div className="flex ">
-
-          <div className="border-r text-center  w-14  border-t flex items-center">
-            <div style={{ transform: 'rotate(-90deg)', width: '60px' }} className="text-center p-0">{month}</div>
-          </div>
-          <div className="col-span-2">
-
-            <p className="p-2  w-72 border-t">Seguridad</p>
-            <p className="p-2 w-72  border-t">Volúmen de producción programada</p>
-            <p className="p-2  w-72  border-t">Fallas B</p>
-            <p className="p-2  w-72  border-t">Fallas C</p>
-            <p className="p-2  w-72  border-t">Material Dañado</p>
-          </div>
+      <div className="grid  grid-cols-6 ">
+        <div className="border-r  p-5 ">{shift}
         </div>
-        <div className={` flex-grow grid `}>
-          <div className="flex  justify-between">
-            {cellBoards}
+        <div className="grid grid-cols-6 border-b border-main_color h-full">
+          <div>
+
+            <div className="border-r border-b justfyce ">Debe
+            </div>
+            <div className="border-r ">Es
+            </div>
           </div>
-          <div className="flex ">
-            {cellBoards}
+          <div className="text-sm text-center flex items-center justify-center  border-main_color py-2 px-3">
+            <p>Presente</p>
+            <p>Ausentess</p>
+            <p>Prestados</p>
+            <p>Capacitacion</p>
           </div>
-          <div className="flex ">
-            {cellBoards}
-          </div>
-          <div className="flex ">
-            {cellBoards}
-          </div>
-          <div className="flex ">
-            {cellBoards}
-          </div>
+
         </div>
       </div>
     </div>
-  );
+  )
 }
