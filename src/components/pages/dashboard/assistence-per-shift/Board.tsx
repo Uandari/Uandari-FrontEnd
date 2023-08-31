3d8099737b4a11d51c3e6dc879ef9ce5222c4b25
Andrés Cordero <andresmcorderor@gmail.com>
2023-08-31 03:11:07 -0600

    feature: assistence per shift in progress

--- src/components/pages/dashboard/assistence-per-shift/Board.tsx
+++ src/components/pages/dashboard/assistence-per-shift/Board.tsx
@@ -1,11 +1,10 @@
 
 import HeadBoard from './modules/HeadBoard';
 import RowBoard from './modules/RowBoard';
-
 const shifts = [
   { title: '1ER TURNO' },
   { title: '2DO TURNO' },
-  { title: '3ER TURNO' },c
+  { title: '3ER TURNO' },
 ];
 
 export default function Board() {
@@ -13,13 +12,13 @@ export default function Board() {
   return (
     
 
-<div className="">
-        <div className="h-36"><HeadBoard /></div>
-    <div className="">{shifts.map((shiftTitle) => (
-          <RowBoard shift={shiftTitle.title} />
-      ))}
-</div>
-</div>
-
-  );
+    <div className="">
+            <div className="h-36"><HeadBoard /></div>
+        <div className="">{shifts.map((shiftTitle) => (
+              <RowBoard shift={shiftTitle.title} />
+          ))}
+    </div>
+    </div>
+    
+      );
 }
