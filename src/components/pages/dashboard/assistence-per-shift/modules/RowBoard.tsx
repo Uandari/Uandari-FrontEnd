32f4806f0840651c85b5da15a73cfebb1d1b2aa3
Andrés Cordero <andresmcorderor@gmail.com>
2023-09-05 12:51:15 -0600

    feature: assistence per shift table in progress

--- src/components/pages/dashboard/assistence-per-shift/modules/RowBoard.tsx
+++ src/components/pages/dashboard/assistence-per-shift/modules/RowBoard.tsx
@@ -6,20 +6,20 @@ export type RowBoardProps = {
 export default function RowBoard({ shift }: RowBoardProps) {
   return (
     <div className="border-b border-main_color ">
-      <div className="grid grid-cols-6 grid-rows-7 ">
+      <div className="grid grid-cols-6 grid-rows-7">
         <div className="col-span-6 ">
-          <div className="grid grid-cols-6 grid-rows-7 ">
-            <div className="row-span-2  border-r text-center p-4 ">{shift}</div>
-            <div className="border-r border-b text-center p-4">Debe</div>
-            <div className=" border-r text-center p-4">Es</div>
-            <div className="col-start-3 row-start-1 border-r  border-b text-center p-4">19</div>
-            <div className="col-start-3 row-start-2 border-r text-center p-4">20</div>
-            <div className="col-start-4 row-start-1 border-r  border-b text-center p-4">21</div>
-            <div className="col-start-4 row-start-2 border-r   text-center p-4">22</div>
-            <div className="col-start-5 row-start-1 border-r  border-b text-center p-4">23</div>
-            <div className="col-start-5 row-start-2 border-r   text-center p-4">24</div>
-            <div className="col-start-6 row-start-1 border-r  border-b text-center p-4">25</div>
-            <div className="col-start-6 row-start-2 border-r  text-center p-4">25</div>
+          <div className="grid grid-cols-6 grid-rows-7">
+            <div className="row-span-2  border-r text-center ">{shift}</div>
+            <div className="border-r border-b text-center">Debe</div>
+            <div className=" border-r text-center ">Es</div>
+            <div className="col-start-3 row-start-1 border-r  border-b text-center ">19</div>
+            <div className="col-start-3 row-start-2 border-r text-center ">20</div>
+            <div className="col-start-4 row-start-1 border-r  border-b text-center">21</div>
+            <div className="col-start-4 row-start-2 border-r   text-center ">22</div>
+            <div className="col-start-5 row-start-1 border-r  border-b text-center ">23</div>
+            <div className="col-start-5 row-start-2 border-r   text-center">24</div>
+            <div className="col-start-6 row-start-1 border-r  border-b text-center">25</div>
+            <div className="col-start-6 row-start-2 border-r  text-center ">25</div>
           </div>
         </div>
       </div>
