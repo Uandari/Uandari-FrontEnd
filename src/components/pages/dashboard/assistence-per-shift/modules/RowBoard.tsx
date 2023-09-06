ecbe3eb3523ddb4c8023947b203dee70cc842398
Andrés Cordero <andresmcorderor@gmail.com>
2023-09-06 11:27:42 -0600

    feature: assistence per shift table in progress

--- src/components/pages/dashboard/assistence-per-shift/modules/RowBoard.tsx
+++ src/components/pages/dashboard/assistence-per-shift/modules/RowBoard.tsx
@@ -9,17 +9,17 @@ export default function RowBoard({ shift }: RowBoardProps) {
       <div className="grid grid-cols-6 grid-rows-7">
         <div className="col-span-6 ">
           <div className="grid grid-cols-6 grid-rows-7">
-            <div className="row-span-2  border-r text-center ">{shift}</div>
-            <div className="border-r border-b text-center">Debe</div>
-            <div className=" border-r text-center ">Es</div>
-            <div className="col-start-3 row-start-1 border-r  border-b text-center ">19</div>
-            <div className="col-start-3 row-start-2 border-r text-center ">20</div>
-            <div className="col-start-4 row-start-1 border-r  border-b text-center">21</div>
-            <div className="col-start-4 row-start-2 border-r   text-center ">22</div>
-            <div className="col-start-5 row-start-1 border-r  border-b text-center ">23</div>
-            <div className="col-start-5 row-start-2 border-r   text-center">24</div>
-            <div className="col-start-6 row-start-1 border-r  border-b text-center">25</div>
-            <div className="col-start-6 row-start-2 border-r  text-center ">25</div>
+            <div className="row-span-2  border-r text-center font-medium text-main_title_color">{shift}</div>
+            <div className="border-r border-b text-center font-medium text-main_title_color">Debe</div>
+            <div className=" border-r text-center font-medium text-main_title_color">Es</div>
+            <div className="col-start-3 row-start-1 border-r font-medium text-main_gray border-b text-center ">19</div>
+            <div className="col-start-3 row-start-2 border-r font-medium text-main_gray text-center ">20</div>
+            <div className="col-start-4 row-start-1 border-r font-medium text-main_gray border-b text-center">21</div>
+            <div className="col-start-4 row-start-2 border-r  font-medium text-main_gray text-center ">22</div>
+            <div className="col-start-5 row-start-1 border-r font-medium text-main_gray border-b text-center ">23</div>
+            <div className="col-start-5 row-start-2 border-r  font-medium text-main_gray text-center">24</div>
+            <div className="col-start-6 row-start-1 border-r font-medium text-main_gray border-b text-center">25</div>
+            <div className="col-start-6 row-start-2 border-r font-medium text-main_gray text-center ">25</div>
           </div>
         </div>
       </div>
