fe94250cdbd555b598d4f46f734126404618e902
Andrés Cordero <andresmcorderor@gmail.com>
2023-09-17 18:38:46 -0600

    feature: base form added

--- src/components/pages/admin-panel/users/modules/FormBoard.tsx
+++ src/components/pages/admin-panel/users/modules/FormBoard.tsx
@@ -5,31 +5,22 @@ import UserIcon from '@icons/iconUser.svg';
 import roleIcon from '@icons/role.svg';
 import { User } from '@interfaces/User';
 import { initialUserInfo } from '@mocks/Users';
-import { Button, Form, Input, Select } from 'antd';
-
-import {
-  usersCreateValidation,
-  usersUpdateValidations,
-} from '../../../../../formValidations/usersValidations';
+import useUsers from '@hooks/useUsers';
+import { usersCreateValidation, usersUpdateValidations } from '../../../../../formValidations/usersValidations';
+import { useEffect } from 'react';
 
 type FormBoardProps = {
   onClose: (callback?: () => void) => void;
   userIdToUpdate?: number;
 };
 
-function FormBoard({ onClose, userIdToUpdate }: FormBoardProps) {
-  const rolesMock = [
-    { value: 1, label: 'Gerente' },
-    { value: 2, label: 'Administrador' },
-    { value: 3, label: 'Coordinador' },
-    { value: 4, label: 'Team Leader' },
-    { value: 5, label: 'Visualizador' },
-  ];
+function FormBoard({ onClose,
+  userIdToUpdate, }: FormBoardProps) {
+
+  const { handleCreateUser, handleUpdateUser } = useUsers();
 
   const handleCreateOrUpdateUser = (state: User) => {
     const userModifiable = { ...state };
-
-    const { handleCreateUser, handleUpdateUser } = useUsers();
 
     if (userIdToUpdate) {
       handleUpdateUser({
@@ -44,65 +35,82 @@ function FormBoard({ onClose, userIdToUpdate }: FormBoardProps) {
     }
     onClose();
   };
-
-  const { errors, handleInputChange, handleSubmit, state, setState, reset } =
+  const { handleInputChange, handleSubmit, state, setState } =
     useForm<User>(
       initialUserInfo,
       userIdToUpdate ? usersUpdateValidations : usersCreateValidation,
       handleCreateOrUpdateUser,
     );
 
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    form.validateFields(['controlNumber', 'name', 'lastNames']);
+  }, [form]);
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <div className="w-[365px]">
+    <Form form={form} initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      style={{ maxWidth: 600 }}
+      layout="vertical">
       <div className="px-5 pt-3">
         <p className="text-black font-medium ">
           {userIdToUpdate ? 'Editar Usuario' : 'Registro de Usuario'}
         </p>
       </div>
-      <div className="pt-6">
-        <Form.Item>
-          <div className="flex items-start gap-2">
-            <img src={controlNumberIcon} alt="problemType-icon" />
-            <Input
-              placeholder="Número de control"
-              id="controlNumber"
-              value={state.controlNumber}
-              name="controlNumber"
-              onChange={(event) => handleInputChange(event)}
-            />
-          </div>
-        </Form.Item>
-        <Form.Item>
-          <div className="flex items-start gap-2">
-            <img src={UserIcon} alt="problemType-icon" />
-            <Input
-              placeholder="Nombre"
-              id="name"
-              value={state.name}
-              name="name"
-              onChange={(event) => handleInputChange(event)}
-            />
-          </div>
-        </Form.Item>
-        <Form.Item>
-          <div className="flex items-start gap-2">
-            <img src={UserIcon} alt="problemType-icon" />
-            <Input
-              placeholder="Apellidos"
-              id="lastnames"
-              value={state.lastnames}
-              name="lastnames"
-              onChange={(event) => handleInputChange(event)}
-            />
-          </div>
-        </Form.Item>
-        <Form.Item>
-          <div className="flex items-center gap-2">
-            <img src={roleIcon} alt="problemType-icon" />
-            <Select defaultValue={rolesMock[0].label} options={rolesMock} />
-          </div>
-        </Form.Item>
-        <div className="flex justify-end">
+      <div className="px-5 pt-5">
+        {/* NOMBRE */}
+        <div className="flex items-start gap-2 w-80">
+          <img className='mt-2 w-5' src={UserIcon} alt="Icono de nombre" />
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: 'Por favor, ingresa los nombres' }]} style={{ width: '100%' }} >
+            <Input type='text' id='name' value={state.name} placeholder='Nombre(s)' name='name' onChange={(event) => handleInputChange(event)} />
+          </Form.Item>
+        </div>
+        {/* APELLIDOS */}
+        <div className="flex items-start gap-2 ">
+          <img className='mt-2 w-5' src={UserIcon} alt="Icono de apellidos" />
+          <Form.Item
+            name="lastNames"
+            rules={[{ required: true, message: 'Por favor, ingresa los apellidos' }]} style={{ width: '100%' }} >
+            <Input type='text' id='lastNames' value={state.lastNames} placeholder='Apellidos' name='lastNames' onChange={(event) => handleInputChange(event)} />
+          </Form.Item>
+        </div>
+        {/* NUMERO DE CONTROL */}
+        <div className="flex items-start gap-2">
+          <img className='mt-2 w-5' src={controlNumberIcon} alt="Icono de número de control" />
+          <Form.Item
+            name="controlNumber"
+            rules={[{ required: true, message: 'Por favor, ingresa el número de control' }]} style={{ width: '100%' }} >
+            <Input type='text' id='controlNumber' value={state.controlNumber} placeholder='Número de control' name='controlNumber' onChange={(event) => handleInputChange(event)} />
+          </Form.Item>
+        </div>
+        {/* ROL */}
+        <div className="flex items-start gap-2 ">
+          <img className='mt-2 w-5' src={roleIcon} alt="Icono de rol" />
+          <Form.Item
+            name="role"
+            rules={[{ required: true, message: 'Por favor, selecciona un rol' }]} style={{ width: '100%' }} >
+            <Select id='role' value={state.role} placeholder='Rol de usuario' onChange={(value) => setState({ ...state, role: value })}> 
+            {roles.map((role) => (
+              <Select.Option key={role} value={role}>
+                {role}
+              </Select.Option>
+            ))}</Select>
+          </Form.Item>
+        </div>
+        <div className="flex justify-end center mt-2">
           <Form.Item>
             <Button
               className="bg-main_blue_dark"
@@ -116,7 +124,7 @@ function FormBoard({ onClose, userIdToUpdate }: FormBoardProps) {
           </Form.Item>
         </div>
       </div>
-    </div>
+    </Form>
   );
 }
 
