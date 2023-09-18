d689f3cc22e4702a143ea6f34b73ea367759d96f
Andrés Cordero <andresmcorderor@gmail.com>
2023-09-17 19:28:12 -0600

    feature: base form added

--- src/components/pages/admin-panel/users/modules/FormBoard.tsx
+++ src/components/pages/admin-panel/users/modules/FormBoard.tsx
@@ -1,8 +1,11 @@
-import useForm from '@hooks/useForm';
-import useUsers from '@hooks/useUsers';
-import controlNumberIcon from '@icons/controlNumber.svg';
-import UserIcon from '@icons/iconUser.svg';
+
+import mailIcon from '@icons/mail.svg';
 import roleIcon from '@icons/role.svg';
+import nameIcon from '@icons/name.svg';
+import passwordIcon from '@icons/password.svg';
+import lastNamesIcon from '@icons/lastNames.svg';
+import controlNumberIcon from '@icons/controlNumber.svg';
+import { Button, Form, Input, Select } from 'antd';
 import { User } from '@interfaces/User';
 import { initialUserInfo } from '@mocks/Users';
 import useUsers from '@hooks/useUsers';
@@ -45,7 +48,7 @@ function FormBoard({ onClose,
   const [form] = Form.useForm();
 
   useEffect(() => {
-    form.validateFields(['controlNumber', 'name', 'lastNames']);
+    form.validateFields(['controlNumber', 'name', 'lastNames', 'mail', 'role']);
   }, [form]);
 
   const onFinish = (values: any) => {
@@ -71,19 +74,19 @@ function FormBoard({ onClose,
       <div className="px-5 pt-5">
         {/* NOMBRE */}
         <div className="flex items-start gap-2 w-80">
-          <img className='mt-2 w-5' src={UserIcon} alt="Icono de nombre" />
+          <img className='mt-2 w-5' src={nameIcon} alt="Icono de nombre" />
           <Form.Item
             name="name"
-            rules={[{ required: true, message: 'Por favor, ingresa los nombres' }]} style={{ width: '100%' }} >
+            rules={[{ required: true, message: 'Por favor, ingresa los nombres', type:'string' }]} style={{ width: '100%' }} >
             <Input type='text' id='name' value={state.name} placeholder='Nombre(s)' name='name' onChange={(event) => handleInputChange(event)} />
           </Form.Item>
         </div>
         {/* APELLIDOS */}
         <div className="flex items-start gap-2 ">
-          <img className='mt-2 w-5' src={UserIcon} alt="Icono de apellidos" />
+          <img className='mt-2 w-5' src={lastNamesIcon} alt="Icono de apellidos" />
           <Form.Item
             name="lastNames"
-            rules={[{ required: true, message: 'Por favor, ingresa los apellidos' }]} style={{ width: '100%' }} >
+            rules={[{ required: true, message: 'Por favor, ingresa los apellidos', type:'string' }]} style={{ width: '100%' }} >
             <Input type='text' id='lastNames' value={state.lastNames} placeholder='Apellidos' name='lastNames' onChange={(event) => handleInputChange(event)} />
           </Form.Item>
         </div>
@@ -92,8 +95,17 @@ function FormBoard({ onClose,
           <img className='mt-2 w-5' src={controlNumberIcon} alt="Icono de número de control" />
           <Form.Item
             name="controlNumber"
-            rules={[{ required: true, message: 'Por favor, ingresa el número de control' }]} style={{ width: '100%' }} >
+            rules={[{ required: true, message: 'Por favor, ingresa el número de control', type:'string' }]} style={{ width: '100%' }} >
             <Input type='text' id='controlNumber' value={state.controlNumber} placeholder='Número de control' name='controlNumber' onChange={(event) => handleInputChange(event)} />
+          </Form.Item>
+        </div>
+        {/* CORREO */}
+        <div className="flex items-start gap-2">
+          <img className='mt-2 w-5' src={mailIcon} alt="Icono de correo" />
+          <Form.Item
+            name="mail"
+            rules={[{ required: true, message: 'Por favor, ingresa una dirección de correo', type:'email'}]} style={{ width: '100%' }} >
+            <Input type='email' id='mail' value={state.mail} placeholder='Correo electrónico' name='mail' onChange={(event) => handleInputChange(event)} />
           </Form.Item>
         </div>
         {/* ROL */}
@@ -101,13 +113,22 @@ function FormBoard({ onClose,
           <img className='mt-2 w-5' src={roleIcon} alt="Icono de rol" />
           <Form.Item
             name="role"
-            rules={[{ required: true, message: 'Por favor, selecciona un rol' }]} style={{ width: '100%' }} >
+            rules={[{ required: true, message: 'Por favor, selecciona un rol', type:'string' }]} style={{ width: '100%' }} >
             <Select id='role' value={state.role} placeholder='Rol de usuario' onChange={(value) => setState({ ...state, role: value })}> 
-            {roles.map((role) => (
+           {/*  {roles.map((role) => (
               <Select.Option key={role} value={role}>
                 {role}
               </Select.Option>
-            ))}</Select>
+            ))} */}</Select>
+          </Form.Item>
+        </div>
+        {/* CONTRASEÑA */}
+        <div className="flex items-start gap-2">
+          <img className='mt-2 w-5' src={passwordIcon} alt="Icono de contraseña" />
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Por favor, ingresa una contraseña', type:'string'}]} style={{ width: '100%' }} >
+            <Input.Password type='password' id='password' value={state.password} placeholder='Contraseña' name='password' onChange={(event) => handleInputChange(event)} />
           </Form.Item>
         </div>
         <div className="flex justify-end center mt-2">
