fb4dd9bd04e146be0842f3da141a2cac3632f258
Andrés Cordero <andres.cordero@cbqasolutions.com>
2023-09-27 02:35:31 -0600

    feature: screen in progress

--- src/components/pages/dashboard/staff-rotation/modules/Board.tsx
+++ src/components/pages/dashboard/staff-rotation/modules/Board.tsx
@@ -1,41 +1,14 @@
-import { useState } from 'react';
-
 import CalendarIcon from '@icons/calender.svg';
-import { goalForYear } from '@mocks/GoalsForYear';
 import { shiftsLabels } from '@mocks/Shifts';
 import { Select } from 'antd';
 
 import RowBoard from './RowBoard';
 
 export default function Board() {
-  const [goalsValues, setGoalsValues] = useState(goalForYear);
-
-  const onChange = (value: string) => {
-    console.log(`selected ${value}`);
-  };
-
-  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget;
-
-    // Actualiza el estado de los objetivos cuando cambia un input
-    setGoalsValues((prevValues) =>
-      prevValues.map((goal) =>
-        goal.name === name ? { ...goal, value: parseInt(value, 10) } : goal,
-      ),
-    );
-  };
-
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    // Evitar que se presione la tecla "e"
-    if (e.key === 'e') {
-      e.preventDefault();
-    }
-  };
-
   return (
     <div className="overflow-y-auto bg-white">
       <div className=" overflow-y-scroll">
-        <div className="flex gap-8 flex-wrap p-6">
+        <div className="flex gap-8 flex-grow p-6">
           <Select
             showSearch
             placeholder="Filtrar por mes"
@@ -66,7 +39,7 @@ export default function Board() {
           />
           <Select
             showSearch
-            placeholder="Filtrar po célula"
+            placeholder="Filtrar por célula"
             optionFilterProp="children"
             filterOption={(input, option) =>
               (option?.label ?? '').includes(input)
@@ -79,26 +52,15 @@ export default function Board() {
             options={shiftsLabels}
           />
         </div>
-        <div className="px-4 py-2 w-full border-y bg-slate-100">
+        <div className="px-6 py-2 w-full border-y bg-slate-100">
           Seleccionar
         </div>
-        <div className="flex px-4 py-2 w-full gap-2">
+        <div className="flex px-6 py-2 w-full border-b gap-2">
           <img src={CalendarIcon} alt="calendar-icon" />
           <p className="text-main_text_color text-sm"> Fecha </p>
         </div>
 
-        <RowBoard
-          help="Seleccionar lo necesario para realizar el proceso / HOE en ella se indica lo necesario para el proceso / En el entorno mirar  medios que no correspondan /Controlar también que los enseres personales (Bolsos, Comida, etc..) no esten fuera de la zona destinada."
-          description="¿Esta la zona libre de piezas y/o objetos innecesarios en la zona de control?"
-        />
-        <div className="py-8 flex justify-end pr-8">
-          <button
-            type="button"
-            className="bg-main_blue_dark text-white rounded-md py-1 px-6"
-          >
-            Guardar checklist
-          </button>
-        </div>
+        <RowBoard />
       </div>
     </div>
   );
--- src/components/pages/dashboard/staff-rotation/modules/RowBoard.tsx
+++ src/components/pages/dashboard/staff-rotation/modules/RowBoard.tsx
@@ -1,44 +1,44 @@
-import { useState } from 'react';
+import { shiftsLabels } from '@mocks/Shifts';
+import { Select } from 'antd';
 
-import InfoIcon from '@assets/icons/infoIcon.svg';
-import { Popover } from 'antd';
-
-type RowReportProp = {
-  description: string;
-  state?: boolean;
-  observation?: string;
-  help: string;
-};
-
-export default function RowBoard({
-  description,
-  help,
-  state,
-  observation,
-}: RowReportProp) {
-  const [yesSelected, setYesSelected] = useState<boolean | null>(
-    state === true ? true : null,
-  );
-  const [noSelected, setNoSelected] = useState<boolean | null>(
-    state === false ? true : null,
-  );
-
-  const handleYesClick = () => {
-    setYesSelected(true);
-    setNoSelected(false);
-  };
-
-  const handleNoClick = () => {
-    setYesSelected(false);
-    setNoSelected(true);
-  };
+export default function RowBoard() {
   return (
-    <div className="grid grid-cols-7 border-y">
-      <div className="col-span-3 p-3 border-r text-main_text_color relative">
-        asfsa
+    <div className="grid grid-cols-7 border-b gap-x-8 px-6 py-3 items-center">
+      <div className="grid grid-rows-2 col-span-2 text-main_text_color relative">
+        <p>Juan Martínez Delgado</p>
+        <p className="text-main_gray">Puesto</p>
       </div>
-      <div className="col-span-1 border-r justify-center items-center grid grid-cols-2">
-        safs
+      <div className="grid col-span1 row-span-2  text-main_text_color relative">
+        <Select
+          showSearch
+          placeholder="Planeado"
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            (option?.label ?? '').includes(input)
+          }
+          filterSort={(optionA, optionB) =>
+            (optionA?.label ?? '')
+              .toLowerCase()
+              .localeCompare((optionB?.label ?? '').toLowerCase())
+          }
+          options={shiftsLabels}
+        />
+      </div>
+      <div className="grid col-span-1  row-span-2  text-main_text_color relative">
+        <Select
+          showSearch
+          placeholder="Real"
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            (option?.label ?? '').includes(input)
+          }
+          filterSort={(optionA, optionB) =>
+            (optionA?.label ?? '')
+              .toLowerCase()
+              .localeCompare((optionB?.label ?? '').toLowerCase())
+          }
+          options={shiftsLabels}
+        />
       </div>
     </div>
   );
