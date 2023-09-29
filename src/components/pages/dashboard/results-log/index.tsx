7a4c996178c56271716fad2cbc3a21869c52c6eb
Andrés Cordero <andres.cordero@cbqasolutions.com>
2023-09-29 06:05:38 -0600

    feature: results log page base layout finished

--- src/components/pages/dashboard/results-log/index.tsx
+++ src/components/pages/dashboard/results-log/index.tsx
@@ -1,13 +1,50 @@
 import Topbar from '@components/layout/topbar';
-import { graphShifts } from '@mocks/GraphShift';
 
 import Graph from './modules/Graph';
 
+const dataMockPieChart = [
+  { name: 'Disponibilidad', value: getRandomValue(1000, 5000) },
+  { name: 'Desempeño', value: getRandomValue(1000, 5000) },
+  { name: 'Calidad', value: getRandomValue(1000, 5000) },
+];
+const dataMockBarChart1 = [
+  { hora: '6:00 - 7:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '7:00 - 8:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '8:00 - 9:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '9:00 - 10:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '10:00 - 11:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '11:00 - 12:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '12:00 - 13:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  { name: '13:00 - 14:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+];
+const dataMockBarChart2 = [
+  { name: 'Disponibilidad', Motores_No_Producidos: getRandomValue(1000, 5000), Motores_Producidos: getRandomValue(1000, 5000) },
+  { name: 'Desempeño', Motores_No_Producidos: getRandomValue(1000, 5000), Motores_Producidos: getRandomValue(1000, 5000) },
+  { name: 'Calidad', Motores_No_Producidos: getRandomValue(1000, 5000), Motores_Producidos: getRandomValue(1000, 5000) },
+];
+const dataMockLineChart = [
+  { name: 'Enero', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Febrero', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Marzo', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Abril', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Mayo', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Junio', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Julio', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Agosto', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Septiembre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Octubre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Noviembre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  { name: 'Diciembre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+];
+
+function getRandomValue(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 export default function ResultsLog() {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col p-">
       <Topbar variant="primary" title="Registro de resultados" />
-      <div className="flex flex-col flex-1 overflow-y-auto bg-main_white border-t border-main_color">
+      <div className="flex flex-col flex-1 overflow-y-auto px-9 py-3 bg-main_white border-t border-main_color">
         <div className="p-4 flex justify-end">
           <button
             className="bg-main_blue_dark py-2 rounded-lg px-6 text-white"
@@ -16,11 +53,10 @@ export default function ResultsLog() {
             Descargar reporte
           </button>
         </div>
-
-        <div className="flex flex-col gap-y-6 pb-7">
+        <div className=" px-4 mb-3  pb-7">
           <Graph
             graph="PieChart"
-            data={graphShifts}
+            data={dataMockPieChart}
             filterDay
             filterWeek
             filterMonth
@@ -28,15 +64,15 @@ export default function ResultsLog() {
             title="Pérdidas generales"
           />
           <Graph
-            graph="BarChart"
-            data={graphShifts}
+            graph="BarChart1"
+            data={dataMockBarChart1}
             filterDay
             filterYear
             title="Desglose de Pérdidas"
           />
           <Graph
-            graph="BarChart"
-            data={graphShifts}
+            graph="BarChart2"
+            data={dataMockBarChart2}
             filterDay
             filterWeek
             filterMonth
@@ -44,16 +80,16 @@ export default function ResultsLog() {
             title="Desglose de Pérdidas"
           />
           <Graph
-            graph="BarChart"
-            data={graphShifts}
+            graph="BarChart1"
+            data={dataMockBarChart1}
             filterWeek
             filterMonth
             filterYear
             title="OEE"
           />
           <Graph
-            graph="LineChart"s
-            data={graphShifts}
+            graph="LineChart"
+            data={dataMockLineChart}
             filterWeek
             filterMonth
             filterYear
--- src/components/pages/dashboard/results-log/modules/Graph.tsx
+++ src/components/pages/dashboard/results-log/modules/Graph.tsx
@@ -2,16 +2,21 @@
 import { monthGoals } from '@mocks/MonthGoals';
 import { weeks } from '@mocks/StaffRotation';
 import { years } from '@mocks/Years';
-import { Select } from 'antd';
+import { Select, Tooltip } from 'antd';
 import {
   ResponsiveContainer,
-  Tooltip,
+  PieChart,
+  Pie,
+  Cell,
+  BarChart,
   CartesianGrid,
   XAxis,
   YAxis,
+  Bar,
   Legend,
+  ComposedChart,
   Line,
-  PieChart,
+  LineChart,
 } from 'recharts';
 
 type GraphProps = {
@@ -19,13 +24,18 @@ type GraphProps = {
   filterWeek?: boolean;
   filterMonth?: boolean;
   filterYear?: boolean;
-  graph: 'PieChart' | 'BarChart' | 'LineChart';
+  graph: 'PieChart' | 'BarChart1' | 'BarChart2' | 'BarChart3' | 'LineChart';
   title: string;
   data: {
-    name: string;
-    Turno_A: number;
-    Turno_B: number;
-    Turno_C: number;
+    name?: string;
+    value?: number;
+    Motores_No_Producidos?: number;
+    Motores_Producidos?: number;
+    Es?: number;
+    Debe?: number;
+    Turno_1?: number;
+    Turno_2?: number;
+    Turno_3?: number;
   }[];
 };
 
@@ -38,80 +48,197 @@ function Graph({
   filterMonth,
   filterYear,
 }: GraphProps) {
+
+  const COLORS = ['#F765A3', '#A155B9', '#16BFD6', '#165BAA'];
   const monthNames = monthGoals.map((goal) => goal.name);
   return (
-    <div>
-      <div className="px-4 mb-3">
-        <p className="text-lg mb-3">{title} </p>
-        {filterDay ? (
-          <Select placeholder="Filtrar por día">
-            {weeks.map((week) => (
-              <div>
-                {week.days.map((day) => (
-                  <Select.Option key={day.date} value={day.date}>
-                    {day.date}
-                  </Select.Option>
+    <div >
+      <p className="text-lg mb-3 font-semibold">{title} </p>
+      {filterDay ? (
+        <Select placeholder="Filtrar por día" className='mx-2'>
+          {weeks.map((week) => (
+            <div>
+              {week.days.map((day) => (
+                <Select.Option key={day.date} value={day.date}>
+                  {day.date}
+                </Select.Option>
+              ))}
+            </div>
+          ))}
+        </Select>
+      ) : null}
+      {filterWeek ? (
+        <Select placeholder="Filtrar por semana" className='mx-2'>
+          {weeks.map((week) => (
+            <Select.Option key={week.value} value={week.value}>
+              {week.value}
+            </Select.Option>
+          ))}
+        </Select>
+      ) : null}
+      {filterMonth ? (
+        <Select placeholder="Filtrar por mes" className='mx-2'>
+          {monthNames.map((name) => (
+            <Select.Option key={name} value={name}>
+              {name}
+            </Select.Option>
+          ))}
+        </Select>
+      ) : null}
+      {filterYear ? (
+        <Select placeholder="Filtrar por año" options={years} className='mx-2' />
+      ) : null}
+      <div className='justify-center items-center pt-8 pb-16 '>
+        {graph === 'BarChart1' ? (
+          <div className='grid grid-rows-6'>
+            <div className='mb-6'>
+              <p className='font-semibold text-main_gray'>Motores No Producidos</p>
+              <p className='font-semibold text-main_blue_dark text-5xl '>5.987,34</p>
+              <p className='text-sm col-span-2'>Motores</p>
+            </div>
+            <div className='row-span-5 max-h-[1200px]'>
+              <ResponsiveContainer height='100%' width='100%'>
+                <ComposedChart
+                  width={500}
+                  height={400}
+                  data={data}
+                  margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 20,
+                  }}
+                >
+                  <CartesianGrid stroke="#f5f5f5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="Es" barSize={100} fill={COLORS[3]} />
+                  <Line dataKey="Debe" stroke={COLORS[1]} />
+                </ComposedChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        ) : graph === 'BarChart2' ? (
+          <div className='grid grid-rows-6'>
+            <div className='mb-6'>
+              <p className='font-semibold text-main_gray'>Motores No Producidos</p>
+              <p className='font-semibold text-main_blue_dark text-5xl '>4,121</p>
+              <p className='text-sm col-span-2'>Motores</p>
+            </div>
+            <div className='row-span-5 max-h-[1200px]'>
+              <ResponsiveContainer height='100%' width='100%'>
+                <BarChart data={data}>
+                <CartesianGrid stroke="#f5f5f5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Legend />
+                  <Bar dataKey="Motores_No_Producidos" stackId="a" fill={COLORS[3]} />
+                  <Bar dataKey="Motores_Producidos" stackId="a" fill={COLORS[1]} />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        ) : graph === 'BarChart3' ? (
+          <div>BarChart3</div>
+        ) : graph === 'PieChart' ? (
+          <div className='grid grid-cols-2'>
+            <div >
+              <ResponsiveContainer height={500}>
+                <PieChart >
+                  <Pie
+                    data={data}
+                    dataKey="value"
+                  >
+                    {data.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                  </Pie>
+
+                </PieChart>
+
+              </ResponsiveContainer>
+              <div className='grid grid-cols-3 text-sm'>
+                {data.map((item, index) => (
+                  <div key={index} className='grid grid-cols-2 '>
+                    <div
+                      className='h-3 w-6 justify-self-end p-2'
+                      style={{ backgroundColor: COLORS[index] }}
+                    />
+                    <p className='justify-self-start px-4'>{item.name}</p>
+                  </div>
                 ))}
               </div>
-            ))}
-          </Select>
-        ) : null}
-        {filterWeek ? (
-          <Select placeholder="Filtrar por semana">
-            {weeks.map((week) => (
-              <Select.Option key={week.value} value={week.value}>
-                {week.value}
-              </Select.Option>
-            ))}
-          </Select>
-        ) : null}
-        {filterMonth ? (
-          <Select placeholder="Filtrar por mes">
-            {monthNames.map((name) => (
-              <Select.Option key={name} value={name}>
-                {name}
-              </Select.Option>
-            ))}
-          </Select>
-        ) : null}
-        {filterYear ? (
-          <Select placeholder="Filtrar por año" options={years} />
-        ) : null}
-      </div>
-      <div className="h-[450px]">
-        <ResponsiveContainer width="99%" height="100%">
-          <div>
-            {graph === 'BarChart' ? (
-              <div>BarChart</div>
-            ) : graph === 'PieChart' ? (
-              <div>PieChart</div>
-            ) : graph === 'LineChart' ? (
-              <div>LineChart</div>
-            ) : null}
+            </div>
+            <div className='grid grid-rows-2 '>
+              <div className='grid grid-cols-3  grid-rows-3 h-fit items-center'>
+                <p className='font-semibold text-main_gray'>Motores Producidos</p>
+                <p className='font-semibold text-main_gray col-span-2'>Motores No Producidos</p>
+                <p className='font-semibold text-main_blue_dark text-5xl'>5.987,34</p>
+                <p className='font-semibold text-main_blue_dark text-5xl col-span-2 '>4122</p>
+                <p className='text-sm '>Motores</p>
+                <p className='text-sm col-span-2'>Motores</p>
+              </div>
+              <div className='grid grid-cols-2 grid-rows-2 h-fit center items-end'>
+                <div className='text-lg font-semibold col-span-2 '>Listado de Problemas</div>
+                <p className='text-main_text_color'>Disponibilidad</p>
+                <p className='text-main_text_color' >5162</p>
+                <p className='text-main_text_color'>Calidad</p>
+                <p className='text-main_text_color'>1254</p>
+                <p className='text-main_text_color'>Desempeño</p>
+                <p className='text-main_text_color'>3521</p>
+              </div>
+            </div>
           </div>
-          {/*  <PieChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="Turno_A" stroke="#07A814" />
-            <Line type="monotone" dataKey="Turno_B" stroke="#1D0F9E" />
-            <Line type="monotone" dataKey="Turno_C" stroke="#CF5221" />
-          </PieChart> */}
-        </ResponsiveContainer>
+        ) : graph === 'LineChart' ? (
+          <div className='grid grid-cols-9'>
+            <div className='row-span-5 max-h-[1200px] col-span-8'>
+              <ResponsiveContainer height='100%' width='100%'>
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={data}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                   <CartesianGrid stroke="#f5f5f5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line dataKey="Turno_1" strokeWidth={3} stroke={COLORS[0]}   activeDot={{ r: 8 }} />
+                  <Line dataKey="Turno_2"  strokeWidth={3} stroke={COLORS[1]} activeDot={{ r: 8 }} />
+                  <Line dataKey="Turno_3" strokeWidth={3} stroke={COLORS[3]}  activeDot={{ r: 8 }} />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+            <div className='grid grid-rows-4 pl-6 gap-4'>
+              <div>
+                <p className='font-semibold'>Meta</p>
+                <p>255</p>
+              </div>
+              <div>
+                <p className='font-semibold'>Promedio turno 1</p>
+                <p>255</p>
+              </div>
+              <div>
+                <p className='font-semibold'>Promedio turno 2</p>
+                <p>255</p>
+              </div>
+              <div>
+                <p className='font-semibold'>Promedio turno 3</p>
+                <p>255</p>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
-    </div>
+    </div >
   );
 }
 
@@ -123,3 +250,5 @@ Graph.defaultProps = {
   filterMonth: false,
   filterYear: false,
 };
+
+
