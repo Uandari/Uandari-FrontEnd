79c4ece871c5dee51f331c9059329d00aa585f16
Andrés Cordero <andres.cordero@cbqasolutions.com>
2023-10-02 05:46:25 -0600

    wip: cells page from admin panel wip

--- src/components/pages/admin-panel/cells/index.tsx
+++ src/components/pages/admin-panel/cells/index.tsx
@@ -1,24 +1,39 @@
+import PlusIcon from '@assets/icons/plus.svg';
 import Topbar from '@components/layout/topbar';
-import Board from './board';
-import RowBoard from './modules/RowBoard';
+import { Cell } from '@interfaces/Cell';
 import { CellsListMock } from '@mocks/Cells';
 
-export default function CellsList() {
+import RowBoard from './RowBoard';
+
+export default function CellsPage() {
   return (
-    <div className="border border-b grid bg-white flex-1 h-screen">
-      <div className="sticky top-0">
-        <Topbar title="Gestión de usuarios" variant="secondary" />
-        <Board>
-      {CellsListMock.map((cell) => (
-        <RowBoard
-          key={cell.id}
-          cellNumber={cell.cellName}
-          line={cell.idLine}
-        />
-      ))}
-    </Board>
+    <div className="flex flex-1 flex-col">
+      <Topbar title="Células" variant="secondary" />
+      <div className="flex flex-1 flex-col border overflow-y-scroll bg-white">
+        <div className="flex justify-between items-center px-4 py-4 border-b">
+          <input
+            placeholder="Buscar"
+            className="border border-main_color pl-3 py-2 w-[300px] rounded-lg outline-none"
+          />
+          <button
+            className="flex items-center gap-x-3 px-6 py-2 bg-main_blue_dark text-white rounded-lg"
+            type="button"
+          >
+            <img className="" src={PlusIcon} alt="plus-icon" />
+            Registrar nueva célula
+          </button>
+        </div>
+
+        <div className="grid grid-cols-3 border-b py-2 px-5 border-main_color">
+          <div className="font-medium text-main_text_color">Número de célula</div>
+          <div className="font-medium text-main_text_color">Team Leader</div>
+          <div className="font-medium text-main_text_color">Línea</div>
+        </div>
+        {CellsListMock.map((cell: Cell) => (
+          <RowBoard cell={cell.cellName} line={cell.line} teamLeader={cell.teamLeader}/>
+
+        ))}
       </div>
     </div>
   );
 }
-
