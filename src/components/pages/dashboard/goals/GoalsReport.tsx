dc6c459dbaba50a2fc7e82a4d9b42252f667c569
Andrés Cordero <andres.cordero@cbqasolutions.com>
2023-10-01 21:41:46 -0600

    fix: 'descargar reporte en PDF' button fixed and function added on all boards that requires it

--- src/components/pages/dashboard/goals/GoalsReport.tsx
+++ src/components/pages/dashboard/goals/GoalsReport.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { goalsStatus } from '@mocks/GoalsStatus';
 import { DatePicker, DatePickerProps } from 'antd';
 import dayjs from 'dayjs';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import DownloadIcon from '@assets/icons/downloadIcon.svg';
 
 export type GoalReportProps = {
   year: string;
@@ -19,8 +22,48 @@ function GoalsReport({ year, setYear }: GoalReportProps) {
     const currentYear = currentDate.getFullYear();
     setYear(String(currentYear));
   }, [setYear]);
+
+  const [btnLoader, setBtnLoader] = useState(false);
+  const [currentWeek, setCurrentWeek] = useState('');
+
+  const handleDownloadPDF = async () => {
+    const capture = document.getElementById('charts');
+    setBtnLoader(true);
+    if (capture) {
+      html2canvas(capture).then((canvas) => {
+        // Obtener la relación de aspecto de las imágenes originales
+        const originalWidth = canvas.width;
+        const originalHeight = canvas.height;
+
+        // Configurar el tamaño del PDF
+        // eslint-disable-next-line new-cap
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const pageWidth = pdf.internal.pageSize.getWidth();
+        const pageHeight = pdf.internal.pageSize.getHeight();
+
+        // Calcular el nuevo tamaño de las imágenes para que se ajusten al PDF
+        let newWidth;
+        let newHeight;
+        if (originalWidth > originalHeight) {
+          newWidth = pageWidth;
+          newHeight = (originalHeight / originalWidth) * newWidth;
+        } else {
+          newHeight = pageHeight;
+          newWidth = (originalWidth / originalHeight) * newHeight;
+        }
+
+        // Agregar la imagen al PDF
+        const imgData = canvas.toDataURL('image/png');
+        pdf.addImage(imgData, 'PNG', 0, 0, newWidth, newHeight);
+
+        // Guardar el PDF
+        setBtnLoader(false);
+        pdf.save(`Pareto_${currentWeek}.pdf`);
+      });
+    }
+  };
   return (
-    <div>
+    <div id='charts'>
       <div className="flex justify-between pb-8">
         <h3 className="text-xl font-medium pb-3">Reporte de metas</h3>
         <DatePicker
@@ -48,10 +91,17 @@ function GoalsReport({ year, setYear }: GoalReportProps) {
         </div>
       ))}
       <button
-        className="bg-main_blue_dark py-2 rounded-lg px-6 text-white"
+        disabled={!(btnLoader === false)}
         type="button"
+        onClick={handleDownloadPDF}
+        className="flex items-center bg-main_blue_dark gap-x-2 text-main_white px-4 py-2 rounded-lg"
       >
-        Descargar reporte
+        <img src={DownloadIcon} alt="download-icon" />
+        {btnLoader ? (
+          <span> Descargando</span>
+        ) : (
+          <span> Descargar reporte en PDF</span>
+        )}
       </button>
     </div>
   );
--- src/components/pages/dashboard/results-log/index.tsx
+++ src/components/pages/dashboard/results-log/index.tsx
@@ -2,6 +2,11 @@ import Topbar from '@components/layout/topbar';
 
 import Graph from './modules/Graph';
 
+import DownloadIcon from '@assets/icons/downloadIcon.svg';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
 const dataMockPieChart = [
   { name: 'Disponibilidad', value: getRandomValue(1000, 5000) },
   { name: 'Desempeño', value: getRandomValue(1000, 5000) },
@@ -41,19 +46,65 @@ function getRandomValue(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 export default function ResultsLog() {
+  const [btnLoader, setBtnLoader] = useState(false);
+  const [currentWeek, setCurrentWeek] = useState('');
+
+  const handleDownloadPDF = async () => {
+    const capture = document.getElementById('charts');
+    setBtnLoader(true);
+    if (capture) {
+      html2canvas(capture).then((canvas) => {
+        // Obtener la relación de aspecto de las imágenes originales
+        const originalWidth = canvas.width;
+        const originalHeight = canvas.height;
+
+        // Configurar el tamaño del PDF
+        // eslint-disable-next-line new-cap
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        const pageWidth = pdf.internal.pageSize.getWidth();
+        const pageHeight = pdf.internal.pageSize.getHeight();
+
+        // Calcular el nuevo tamaño de las imágenes para que se ajusten al PDF
+        let newWidth;
+        let newHeight;
+        if (originalWidth > originalHeight) {
+          newWidth = pageWidth;
+          newHeight = (originalHeight / originalWidth) * newWidth;
+        } else {
+          newHeight = pageHeight;
+          newWidth = (originalWidth / originalHeight) * newHeight;
+        }
+
+        // Agregar la imagen al PDF
+        const imgData = canvas.toDataURL('image/png');
+        pdf.addImage(imgData, 'PNG', 0, 0, newWidth, newHeight);
+
+        // Guardar el PDF
+        setBtnLoader(false);
+        pdf.save(`Pareto_${currentWeek}.pdf`);
+      });
+    }
+  };
   return (
     <div className="flex flex-1 flex-col p-">
-      <Topbar title="Registro de resultados" underline/>
+      <Topbar title="Registro de resultados" underline />
       <div className="flex flex-col flex-1 overflow-y-auto px-9 py-3 bg-main_white">
         <div className="p-4 flex justify-end">
           <button
-            className="bg-main_blue_dark py-2 rounded-lg px-6 text-white"
+            disabled={!(btnLoader === false)}
             type="button"
+            onClick={handleDownloadPDF}
+            className="flex items-center bg-main_blue_dark gap-x-2 text-main_white px-4 py-2 rounded-lg"
           >
-            Descargar reporte
+            <img src={DownloadIcon} alt="download-icon" />
+            {btnLoader ? (
+              <span> Descargando</span>
+            ) : (
+              <span> Descargar reporte en PDF</span>
+            )}
           </button>
         </div>
-        <div className=" px-4 mb-3  pb-7">
+        <div id='charts' className=" px-4 mb-3  pb-7">
           <Graph
             graph="PieChart"
             data={dataMockPieChart}
