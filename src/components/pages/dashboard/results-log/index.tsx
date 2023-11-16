3112d6a3ca2ef4d30eceae7341d9fedfd56a78bf
Ricardo-CBQA <140541333+Ricardo-CBQA@users.noreply.github.com>
2023-11-15 19:14:21 -0600

    fix: numbers in result log

--- src/components/pages/dashboard/results-log/index.tsx
+++ src/components/pages/dashboard/results-log/index.tsx
@@ -8,33 +8,137 @@ const dataMockPieChart = [
   { name: 'Calidad', value: getRandomValue(1000, 5000) },
 ];
 const dataMockBarChart1 = [
-  { hora: '6:00 - 7:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '7:00 - 8:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '8:00 - 9:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '9:00 - 10:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '10:00 - 11:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '11:00 - 12:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '12:00 - 13:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
-  { name: '13:00 - 14:00', Es: getRandomValue(1000, 5000), Debe: getRandomValue(1000, 5000) },
+  {
+    hora: '6:00 - 7:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '7:00 - 8:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '8:00 - 9:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '9:00 - 10:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '10:00 - 11:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '11:00 - 12:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '12:00 - 13:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
+  {
+    name: '13:00 - 14:00',
+    Es: getRandomValue(1000, 5000),
+    Debe: getRandomValue(1000, 5000),
+  },
 ];
 const dataMockBarChart2 = [
-  { name: 'Disponibilidad', Motores_No_Producidos: getRandomValue(1000, 5000), Motores_Producidos: getRandomValue(1000, 5000) },
-  { name: 'Desempeño', Motores_No_Producidos: getRandomValue(1000, 5000), Motores_Producidos: getRandomValue(1000, 5000) },
-  { name: 'Calidad', Motores_No_Producidos: getRandomValue(1000, 5000), Motores_Producidos: getRandomValue(1000, 5000) },
+  {
+    name: 'Disponibilidad',
+    Motores_No_Producidos: getRandomValue(1000, 5000),
+    Motores_Producidos: getRandomValue(1000, 5000),
+  },
+  {
+    name: 'Desempeño',
+    Motores_No_Producidos: getRandomValue(1000, 5000),
+    Motores_Producidos: getRandomValue(1000, 5000),
+  },
+  {
+    name: 'Calidad',
+    Motores_No_Producidos: getRandomValue(1000, 5000),
+    Motores_Producidos: getRandomValue(1000, 5000),
+  },
 ];
 const dataMockLineChart = [
-  { name: 'Enero', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Febrero', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Marzo', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Abril', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Mayo', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Junio', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Julio', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Agosto', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Septiembre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Octubre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Noviembre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
-  { name: 'Diciembre', Turno_1: getRandomValue(1000, 5000), Turno_2: getRandomValue(2000, 6000), Turno_3: getRandomValue(3000, 7000) },
+  {
+    name: 'Enero',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Febrero',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Marzo',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Abril',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Mayo',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Junio',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Julio',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Agosto',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Septiembre',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Octubre',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Noviembre',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
+  {
+    name: 'Diciembre',
+    Turno_1: getRandomValue(1000, 5000),
+    Turno_2: getRandomValue(2000, 6000),
+    Turno_3: getRandomValue(3000, 7000),
+  },
 ];
 
 function getRandomValue(min: number, max: number) {
